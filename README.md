da623098bdbdccb6c440b52c8ba2bd714693dccf
schakradari <saisindhu_chakradari@intuit.com>
2023-02-08 17:10:32 -0500

    Changes in the path from /api/extension to /extensions
    Signed-off-by: schakradari <saisindhu_chakradari@intuit.com>

--- README.md
+++ README.md
@@ -1,16 +1,23 @@
 # ArgoCD Extension Metrics
+
 The project introduces the ArgoCD extension to enable Metrics on Resource tab.
 ![](./docs/images/screenshot.png)
+
 # Quick Start
 
 - Install Argo CD and Argo CD Extensions Controller: https://github.com/argoproj-labs/argocd-extensions
 - Create `argocd-metrics-server` deployment in `argocd` namespace
+
 ```sh
 kubectl apply -n argocd \
     -f https://raw.githubusercontent.com/argoproj-labs/argocd-extension-metrics/main/manifests/install.yaml
 
+kubectl apply -n argocd https://raw.githubusercontent.com/argoproj-labs/argocd-extension-metrics/main/manifests/configmap.yaml
+
 ```
+
 - Create `argocd-extension-metrics` extension in `argocd` namespace
+
 ```
 kubectl apply -n argocd \
     -f https://raw.githubusercontent.com/argoproj-labs/argocd-extension-metrics/main/manifests/extension.yaml
@@ -19,28 +26,30 @@ kubectl apply -n argocd \
 # Enable the Argo UI to access the ArgoCD Metrics Server.
 
 ## ArgoCD < v2.5
+
 ArgoCD version less than v2.5 doesn't support the `Backend Proxy`. You have to configure the Ingress to deviate the API calls between ArgoCD server and ArgoCD metrics Server
+
 ```yaml
 spec:
   rules:
-  - http:
-      paths:
-      - backend:
-          service:
-            name: argocd-o11y-server
-            port:
-              number: 9003
-        path: /api/extension/metrics
-        pathType: Prefix
-      - backend:
-          service:
-            name: argocd-server
-            port:
-              number: 80
-        path: /
-        pathType: Prefix
-
+    - http:
+        paths:
+          - backend:
+              service:
+                name: argocd-o11y-server
+                port:
+                  number: 9003
+            path: /extensions/metrics
+            pathType: Prefix
+          - backend:
+              service:
+                name: argocd-server
+                port:
+                  number: 80
+            path: /
+            pathType: Prefix
 ```
 
 ## ArgoCD > v2.6
+
 ArgoCD Team is working to implemented backend proxy [proposal](https://github.com/argoproj/argo-cd/blob/master/docs/proposals/proxy-extensions.md)
--- extensions/resource-metrics/resource-metrics-extention/ui/src/index.tsx
+++ extensions/resource-metrics/resource-metrics-extention/ui/src/index.tsx
@@ -1,90 +1,131 @@
-import * as React from 'react'
-import { useEffect, useState } from 'react'
-import Metrics from './Metrics/Metrics'
-import './styles.scss'
+import * as React from "react";
+import { useEffect, useState } from "react";
+import Metrics from "./Metrics/Metrics";
+import "./styles.scss";
 
 export const roundNumber = (num: number, dig: number): number => {
-  return Math.round(num*10**dig) / 10**dig;
-}
+  return Math.round(num * 10 ** dig) / 10 ** dig;
+};
 
 export const Extension = (props: any) => {
-  const [events, setEvents] = useState([])
-  const [duration, setDuration] = useState("1h")
-  const [hasMetrics, setHasMetrics] = useState<boolean>(false)
-  const [isLoading, setIsLoading] = useState<boolean>(true)
+  const [events, setEvents] = useState([]);
+  const [duration, setDuration] = useState("1h");
+  const [hasMetrics, setHasMetrics] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
-  const loc = window.location
-  const { resource, application } = props
-  const application_name = application?.metadata?.name || ''
+  const loc = window.location;
+  const { resource, application } = props;
+  const application_name = application?.metadata?.name || "";
 
   const updateDuration = (e: any, dur: string) => {
     e.preventDefault();
     setDuration(dur);
-  }
+  };
   useEffect(() => {
-    let url = `/api/v1/applications/${application_name}/events?resourceUID=${resource.metadata.uid}&resourceNamespace=${resource.metadata.namespace}&resourceName=${resource.metadata.name}&duration=${duration}`
-    if (resource.kind === 'Application'){
-      url = `/api/v1/applications/${application_name}/events`
+    let url = `/api/v1/applications/${application_name}/events?resourceUID=${resource.metadata.uid}&resourceNamespace=${resource.metadata.namespace}&resourceName=${resource.metadata.name}&duration=${duration}`;
+    if (resource.kind === "Application") {
+      url = `/api/v1/applications/${application_name}/events`;
     }
     fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        setEvents(data?.items || [])
-      }).catch(err => {
-        console.error('res.data', err)
+      .then((response) => response.json())
+      .then((data) => {
+        setEvents(data?.items || []);
+      })
+      .catch((err) => {
+        console.error("res.data", err);
       });
-  }, [application_name, resource, duration])
+  }, [application_name, resource, duration]);
 
   return (
     <React.Fragment>
-      {!isLoading && !hasMetrics &&
-        <p>No metrics to display</p>
-      }
-      {!isLoading && hasMetrics && <div className='application-metrics__MetricsDurationSelector'>
-        <a
-          href={`${loc}`}
-          className={`application-metrics__MetricsDuration ${duration === '24h' ? 'active' : ''}`}
-          onClick={(e)=>{updateDuration(e, "24h")}}>
+      {!isLoading && !hasMetrics && <p>No metrics to display</p>}
+      {!isLoading && hasMetrics && (
+        <div className="application-metrics__MetricsDurationSelector">
+          <a
+            href={`${loc}`}
+            className={`application-metrics__MetricsDuration ${
+              duration === "24h" ? "active" : ""
+            }`}
+            onClick={(e) => {
+              updateDuration(e, "24h");
+            }}
+          >
             1 day
-        </a>
-        <a
-          href={`${loc}`}
-          className={`application-metrics__MetricsDuration ${duration === '12h' ? 'active' : ''}`}
-          onClick={(e)=>{updateDuration(e, "12h")}}
-        >
+          </a>
+          <a
+            href={`${loc}`}
+            className={`application-metrics__MetricsDuration ${
+              duration === "12h" ? "active" : ""
+            }`}
+            onClick={(e) => {
+              updateDuration(e, "12h");
+            }}
+          >
             12 hrs
-        </a>
-        <a
-          href={`${loc}`}
-          className={`application-metrics__MetricsDuration ${duration === '6h' ? 'active' : ''}`}
-          onClick={(e)=>{updateDuration(e, "6h")}}
-        >
+          </a>
+          <a
+            href={`${loc}`}
+            className={`application-metrics__MetricsDuration ${
+              duration === "6h" ? "active" : ""
+            }`}
+            onClick={(e) => {
+              updateDuration(e, "6h");
+            }}
+          >
             6 hrs
-        </a>
-        <a
-          href={`${loc}`}
-          className={`application-metrics__MetricsDuration ${duration === '2h' ? 'active' : ''}`}
-          onClick={(e)=>{updateDuration(e, "2h")}}
-        >
+          </a>
+          <a
+            href={`${loc}`}
+            className={`application-metrics__MetricsDuration ${
+              duration === "2h" ? "active" : ""
+            }`}
+            onClick={(e) => {
+              updateDuration(e, "2h");
+            }}
+          >
             2 hrs
-        </a>
-        <a
-          href={`${loc}`}
-          className={`application-metrics__MetricsDuration ${duration === '1h' ? 'active' : ''}`}
-          onClick={(e)=>{updateDuration(e, "1h")}}
-        >
+          </a>
+          <a
+            href={`${loc}`}
+            className={`application-metrics__MetricsDuration ${
+              duration === "1h" ? "active" : ""
+            }`}
+            onClick={(e) => {
+              updateDuration(e, "1h");
+            }}
+          >
             1 hr
-        </a>
-      </div>}
-      <Metrics {...props} events={events} duration={duration} setHasMetrics={setHasMetrics} isLoading={isLoading} setIsLoading={setIsLoading}/>
+          </a>
+        </div>
+      )}
+      <Metrics
+        {...props}
+        events={events}
+        duration={duration}
+        setHasMetrics={setHasMetrics}
+        isLoading={isLoading}
+        setIsLoading={setIsLoading}
+      />
     </React.Fragment>
-  )
-}
+  );
+};
 
 export const component = Extension;
 
 // Register the component extension in ArgoCD
 ((window: any) => {
-  window?.extensionsAPI?.registerResourceExtension(component, '*', '*', 'Metrics', {icon: "fa fa-chart-area"});
-  window?.extensionsAPI?.registerResourceExtension(component, '', 'Pod', 'Metrics', {icon: "fa fa-chart-area"});
-})(window)
+  window?.extensionsAPI?.registerResourceExtension(
+    component,
+    "*",
+    "*",
+    "Metrics",
+    { icon: "fa fa-chart-area" }
+  );
+  window?.extensionsAPI?.registerResourceExtension(
+    component,
+    "",
+    "Pod",
+    "Metrics",
+    { icon: "fa fa-chart-area" }
+  );
+})(window);
